a08e4f279b7d32d4b2ec59d56e9eae6e498c85d5
Will Gonzalez <will.colocho@gmail.com>
2020-08-04 04:33:57 +0000

    Automatically backed up by Learn

--- learning_comments.rb
+++ learning_comments.rb
@@ -5,3 +5,11 @@ if run_code_inside
 end
 puts "Code after if...end"
 
+chance_of_rain = 0.2
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+  puts "Pack an umbrella!"
+else
+  puts "Stay home and read Hegel."
+end
